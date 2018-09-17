4b14ec071faa29e5d39e6fda871189bee270f073
Danielle Metzner <danimetzner@gmail.com>
2018-09-17 16:53:37 -0700

    Added show method to the task controllers and added the show.html and routed it in the route.rb file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,17 @@
-TASKS = [ {name: "Task 1", description: "CS Fundamentals HW", completion_date: "October 4, 2018"},
-          {name: "Task 2", description: "Ruby Rails - TaskList", completion_date: "October 6, 2018"},
-          {name: "Task 3", description: "Personal Website", completion_date: "October 9, 2018"}]
+TASKS = [ {id: 1, name: "Task 1", description: "CS Fundamentals HW", completion_date: "October 4, 2018"},
+          {id: 2, name: "Task 2", description: "Ruby Rails - TaskList", completion_date: "October 6, 2018"},
+          {id: 3, name: "Task 3", description: "Personal Website", completion_date: "October 9, 2018"}]
 
 class TasksController < ApplicationController
   def index
     @tasks = TASKS
   end
+
+  def show
+    id = params[:id]
+    @task = TASKS.find do |task|
+      task[:id] == id.to_i
+    end
+  end
+
 end
