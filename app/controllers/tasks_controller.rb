ce90ceb661aac4f9e7683f002821e8e62461f40f
Danielle Metzner <danimetzner@gmail.com>
2018-09-17 14:30:39 -0700

    Added tasks to my controller.rb file for tasks and printed out the tasks using the tasks/index.html.erb file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
+TASKS = [ {name: "Task 1", description: "CS Fundamentals HW", completion_date: "October 4, 2018"},
+          {name: "Task 2", description: "Ruby Rails - TaskList", completion_date: "October 6, 2018"},
+          {name: "Task 3", description: "Personal Website", completion_date: "October 9, 2018"}]
+
 class TasksController < ApplicationController
   def index
+    @tasks = TASKS
   end
 end
