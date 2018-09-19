dbedc08f17db06857ff1d7e2d6e8b3a2a40571de
Danielle Metzner <danimetzner@gmail.com>
2018-09-19 11:19:26 -0700

    Completed wave-1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,15 @@
-TASKS = [ {id: 1, name: "Task 1", description: "CS Fundamentals HW", completion_date: "October 4, 2018"},
-          {id: 2, name: "Task 2", description: "Ruby Rails - TaskList", completion_date: "October 6, 2018"},
-          {id: 3, name: "Task 3", description: "Personal Website", completion_date: "October 9, 2018"}]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all.order(:completion_date)
   end
 
   def show
     id = params[:id]
-    @task = TASKS.find do |task|
-      task[:id] == id.to_i
+    @task = Task.find_by(id: id)
+
+    if @task.nil?
+     render :notfound, status: :not_found
     end
-  end
 
 end
